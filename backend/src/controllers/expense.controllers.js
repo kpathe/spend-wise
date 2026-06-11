540073abfe29dbd9965a1fa0e8874eb0e6dc71af
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-11 07:19:55 +0530

    feat: implement expense editing functionality with validation

--- backend/src/controllers/expense.controllers.js
+++ backend/src/controllers/expense.controllers.js
@@ -31,4 +31,68 @@ const handleCreateExpense = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { expense }, "Expense created successfully!"));
 });
 
-export { handleCreateExpense };
+const handleEditExpense = asyncHandler(async (req, res) => {
+  const { name, amount, type, category, date, note } = req.body;
+
+  if (!name && !amount)
+    throw new ApiError(400, "Atleast one field is required");
+
+  const updates = {};
+
+  if (name !== undefined) {
+    if (!name.trim()) {
+      throw new ApiError(400, "Name cannot be empty");
+    }
+    updates.name = name;
+  }
+
+  if (amount !== undefined) {
+    if (amount <= 0) {
+      throw new ApiError(400, "Amount must be greater than 0");
+    }
+    updates.amount = amount;
+  }
+
+  if (category !== undefined) {
+    updates.category = category;
+  }
+
+  if (type !== undefined) {
+    if (type !== "credit" || type !== "debit") {
+      throw new ApiError(400, "Enter a valid transaction type");
+    }
+    updates.transactionType = type;
+  }
+
+  if (date !== undefined) {
+    updates.date = date;
+  }
+
+  if (note !== undefined) {
+    if (!note.trim()) {
+      throw new ApiError(400, "Note cannot be empty");
+    }
+
+    updates.note = note;
+  }
+
+  if (Object.keys(updates).length === 0) {
+    throw new ApiError(400, "No valid fields provided for update");
+  }
+
+  const expense = await Expense.findByIdAndUpdate(
+    req.params.expenseId,
+    { $set: updates },
+    { new: true },
+  );
+
+  if (!expense) throw new ApiError(204, "Error updating expense");
+
+  console.log(expense);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, expense, "Expense updated successfully!"));
+});
+
+export { handleCreateExpense, handleEditExpense };
--- backend/src/routes/expense.routes.js
+++ backend/src/routes/expense.routes.js
@@ -1,9 +1,13 @@
-import { handleCreateExpense } from "../controllers/expense.controllers.js";
+import {
+  handleCreateExpense,
+  handleEditExpense,
+} from "../controllers/expense.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
 
 const expenseRouter = Router();
 
 expenseRouter.post("/create", verifyJWT, handleCreateExpense);
+expenseRouter.patch("/edit/:expenseId", verifyJWT, handleEditExpense);
 
 export default expenseRouter;
