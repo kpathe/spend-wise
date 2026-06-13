4bcbc7a6b00b0df8c7d241a76fbfcee9e924dace
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-13 16:00:00 +0530

    feat: add handlers for fetching categories and a specific category

--- backend/src/controllers/category.controllers.js
+++ backend/src/controllers/category.controllers.js
@@ -2,6 +2,7 @@ import { Category } from "../models/category.models.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
 
 const handleCreateCategory = asyncHandler(async (req, res) => {
   const { name, type } = req.body;
@@ -30,4 +31,27 @@ const handleCreateCategory = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, category, "Category created successfully!"));
 });
 
-export { handleCreateCategory };
+const handleGetCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find({});
+  if (!categories) throw new ApiError(404, "Categories not found!");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, categories, "Categories fetched successfully!"));
+});
+
+const handleGetCategory = asyncHandler(async (req, res) => {
+  const { categoryId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(categoryId))
+    throw new ApiError(400, "Invalid categoryId");
+
+  const category = await Category.findOne({ _id: categoryId });
+
+  if (!category) throw new ApiError(404, "Category not found!");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, category, "Category fetched successfully"));
+});
+
+export { handleCreateCategory, handleGetCategories, handleGetCategory };
