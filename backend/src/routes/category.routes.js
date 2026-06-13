46ce83248b98afd03ac4050e48b731c462b92cf4
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-13 15:59:50 +0530

    refactor: uncomment category route handlers in category router

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,15 +1,15 @@
 import {
   handleCreateCategory,
-  //   handleGetCategory,
-  //   handleGetCategories,
+  handleGetCategory,
+  handleGetCategories,
 } from "../controllers/category.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
 
 const categoryRouter = Router();
 
-// categoryRouter.get("/:categoryId", handleGetCategory);
-// categoryRouter.get("/", handleGetCategories);
+categoryRouter.get("/:categoryId", handleGetCategory);
+categoryRouter.get("/", handleGetCategories);
 categoryRouter.post("/", handleCreateCategory);
 
 export default categoryRouter;
