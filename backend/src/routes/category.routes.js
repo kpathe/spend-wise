be1dafbbb11fc0936a412aa8ab2920241c90e294
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-13 15:45:57 +0530

    feat: add category routes for creating categories

---
+++ backend/src/routes/category.routes.js
@@ -0,0 +1,15 @@
+import {
+  handleCreateCategory,
+  //   handleGetCategory,
+  //   handleGetCategories,
+} from "../controllers/category.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { Router } from "express";
+
+const categoryRouter = Router();
+
+// categoryRouter.get("/:categoryId", handleGetCategory);
+// categoryRouter.get("/", handleGetCategories);
+categoryRouter.post("/", handleCreateCategory);
+
+export default categoryRouter;
