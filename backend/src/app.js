4d6a7f54956ecabadc7711ba4563cf7ae62afbfe
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-13 15:46:10 +0530

    feat: add category routes to main application file

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import userRouter from "./routes/user.routes.js";
 import expenseRouter from "./routes/expense.routes.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
+import categoryRouter from "./routes/category.routes.js";
 const app = express();
 
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/expenses", expenseRouter);
-// app.use("/api/category", categoryRouter);
+app.use("/api/categories", categoryRouter);
+
 
 export { app };
