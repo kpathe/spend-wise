47e5e8dec4c7ead9d92298b22eb4dc0712c43d41
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-14 16:35:06 +0530

    feat: implement initial App component structure and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-5xl text-center">Welcome to Spendwise</h1>
+
+      <div className="m-10 text-center">
+        <p>The App is under development...</p>
+        <p>Will be launched soon!</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
