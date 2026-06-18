a42a2e8d34c01333b255906a1580b11557f12abb
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-18 23:08:47 +0530

    feat: implement routing for authentication and main application layouts

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,57 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "../pages/auth/Login";
+import Signup from "../pages/auth/Signup";
+import Dashboard from "../pages/dashboard/Dashboard";
+import AllExpenses from "../pages/expenses/AllExpenses";
+import DailyExpenses from "../pages/expenses/DailyExpenses";
+import WeeklyExpenses from "../pages/expenses/WeeklyExpenses";
+import MonthlyExpenses from "../pages/expenses/MonthlyExpenses";
+import YearlyExpenses from "../pages/expenses/YearlyExpenses";
+import Categories from "../pages/categories/Categories";
+import CategoryBreakdown from "../pages/categories/CategoryBreakdown";
+import AuthLayout from "../layouts/AuthLayout";
+import MainLayout from "../layouts/MainLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login />,
+      },
+      {
+        path: "/auth/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/expense/daily-expenses", element: <DailyExpenses /> },
+      { path: "/expense/all-expenses", element: <AllExpenses /> },
+      { path: "/expense/weekly-expenses", element: <WeeklyExpenses /> },
+      { path: "/expense/monthly-expenses", element: <MonthlyExpenses /> },
+      { path: "/expense/yearly-expenses", element: <YearlyExpenses /> },
+      { path: "/categories/", element: <Categories /> },
+      { path: "/categories/breakdown", element: <CategoryBreakdown /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/auth/login" replace />,
+  },
+]);
+
+function AppRoutes() {
+  return <RouterProvider router={router} />;
+}
+
+export default AppRoutes;
