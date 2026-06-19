a1ae1754b92d7051015dedef93780710790c1044
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-19 15:57:12 +0530

    refactor: remove unused imports from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
-import "./index.css";
 import App from "./App.jsx";
-import Login from "../src/pages/auth/Login.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
