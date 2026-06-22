0c05f4eeb5bd94ebfaf8d995934aca5b3b37dcca
Kaushlendra Pathe <kpathe972@gmail.com>
2026-06-22 11:25:15 +0530

    fix: correct endpoint for creating an expense and ensure consistency in expense API methods

--- frontend/src/api/expense.api.js
+++ frontend/src/api/expense.api.js
@@ -1,8 +1,39 @@
 import apiClient from "../services/axios";
 
 export const createExpense = async (formData) => {
-  const response = await apiClient.post("/expense", formData);
+  const response = await apiClient.post("/expenses", formData);
   return response.data;
 };
 
-export const getExpense = async (id)
+export const getExpense = async (expenseId) => {
+  const response = await apiClient.get("/expenses", expenseId);
+  return response.data;
+};
+
+export const editExpense = async (expenseId, updatedData) => {
+  const response = await apiClient.patch(`/expenses/${expenseId}`, updatedData);
+
+  return response.data;
+};
+
+export const deleteExpense = async (expenseId) => {
+  const response = await apiClient.delete(`/expenses/${expenseId}`);
+
+  return response.data;
+};
+
+export const getExpenses = async (filterObject) => {
+  const response = await apiClient.get("/expenses", {
+    params: filterObject,
+  });
+
+  return response.data;
+};
+
+export const getExpenseSummary = async (filterObject) => {
+  const response = await apiClient.get("/expenses/summary", {
+    params: filterObject,
+  });
+
+  return response.data;
+};
